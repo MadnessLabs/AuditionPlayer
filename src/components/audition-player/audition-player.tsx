823f958b1d46c80c100d377667c5d0c8a805f3b1
Bobby Johnson <bobby@madnesslabs.net>
2018-01-30 01:49:47 -0600

    Add toggling of play / pause

--- src/components/audition-player/audition-player.tsx
+++ src/components/audition-player/audition-player.tsx
@@ -2,24 +2,25 @@ import { Component, Prop, Element } from '@stencil/core';
 
 @Component({
   tag: 'audition-player',
-  styleUrl: 'audition-player.scss',
-  shadow: true
+  styleUrl: 'audition-player.scss'
 })
 export class AuditionPlayer {
   @Element() auditionPlayerEl: HTMLElement;
   @Prop() src: string;
 
   playAudition(event) {
-    console.dir(this.auditionPlayerEl);
     var audioEl: HTMLAudioElement = this.auditionPlayerEl.querySelector('audio');
-    console.log(audioEl);
-    audioEl.play();
+    var buttonEl: HTMLElement = this.auditionPlayerEl.querySelector('button');
+    var isPaused = buttonEl.classList.contains('paused');
+
+    isPaused ? buttonEl.classList.remove('paused') : buttonEl.classList.add('paused');
+    isPaused ? audioEl.pause() : audioEl.play();
   }
 
   render() {
     return (
       <a onClick={(event: UIEvent) => this.playAudition(event)}>
-        <button class='button'></button>
+        <button></button>
         <audio>
           <source src={this.src} type="audio/mpeg" />
           Your browser does not support the audio element.
